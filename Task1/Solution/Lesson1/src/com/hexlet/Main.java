3c910faf067db1fffda3e35159ef9dda47b46827
Viacheslav Goroveckiy <vgoroveckiy@gmail.com>
2014-08-28 23:33:01 +0300

    2222

---
+++ Task1/Solution/Lesson1/src/com/hexlet/Main.java
@@ -0,0 +1,18 @@
+package com.hexlet;
+
+import com.hexlet.humans.Student;
+
+import com.hexlet.common.Course;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student studentMax = new Student();
+        studentMax.course.name = "Java course";
+        studentMax.name = "Max";
+        studentMax.sayHello();
+
+    }
+
+    }
---
+++ Task1/Solution/Lesson1/src/com/hexlet/humans/Human.java
@@ -0,0 +1,11 @@
+package com.hexlet.humans;
+
+public class Human {
+
+    public String name;
+
+    public String getName() {
+
+        return name;
+    }
+}
