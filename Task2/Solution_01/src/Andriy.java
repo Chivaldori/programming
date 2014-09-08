52db68617285ee28c4054591e6390af9e6055409
Enrico Chivaldori <echivaldori@gmail.com>
2014-09-08 20:43:48 +0300

    Пример когда у Андрея есть 2 машины.

--- Task2/Solution_01/src/Andriy.java
+++ Task2/Solution_01/src/Andriy.java
@@ -1,5 +1,15 @@
 public class Andriy extends Human{
 
+	Car lanos, leksus;
+	
+	public Andriy(){
+		lanos = new Car();
+		laons.model = "lanos";
+		
+		leksus = new Car();
+		leksus.model = "leksus";
+	}
+
     public void feedFishes() {
     }
 
