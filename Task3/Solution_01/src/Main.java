9c85731cc37f5698082e706bc2c7c0202eb616a0
Enrico Chivaldori <echivaldori@gmail.com>
2014-09-16 22:54:56 +0300

    Зачёт. Решение с кодировками приложил, просто немного другой метод чтения потока из файла.
    A вот тебе лаконичность C#:
    
    String[] fullArray = File.ReadAllLines("full.txt");
    String[] partialArray = File.ReadAllLines("part.txt");
    var diff = fullArray.Where(x => !partialArray.Contains(x));
    File.WriteAllLines("result.txt", diff);

--- Task3/Solution_01/src/Main.java
+++ Task3/Solution_01/src/Main.java
@@ -4,14 +4,21 @@ import java.util.ArrayList;
 public class Main {
 
         public static void main(String[] args) throws IOException {
+            // странновато выглядят пути с обратными слешами. по-линуксовски
             String sourceFileNameFull = "C://GIT.REPO/Task3/full.txt";
             String destFileNameResult = "C://GIT.REPO/Task3/result.txt";
             String sourceFileNamePart = "C://GIT.REPO/Task3/part.txt";
 
-            ArrayList<String> arrInFull = new ArrayList<String>(ReadFromFile.read(sourceFileNameFull));
-            ArrayList<String> arrInPart = new ArrayList<String>(ReadFromFile.read(sourceFileNamePart));
+            //ArrayList<String> arrInFull = new ArrayList<String>(ReadFromFile.read(sourceFileNameFull));
+            //ArrayList<String> arrInPart = new ArrayList<String>(ReadFromFile.read(sourceFileNamePart));
+            // Создаёшь один объект списка, а в качестве параметра его конструктора передаешь ему другой объект списка,
+            // созданный в методе ReadFromFile. Только лишнюю память мучаешь. Метод и так создаёт и возвращает список, так что нужно было написать так:
+            ArrayList<String> arrInFull = ReadFromFile.read(sourceFileNameFull);
+            ArrayList<String> arrInPart = ReadFromFile.read(sourceFileNamePart);
+
             ArrayList<String> arrOut = new ArrayList<String>();
 
+            // У меня тут программа повалилась по NullReferenceException, см. комментарии в методе ReadFromFile()
             for (String n : arrInFull) {
                     if (!arrInPart.contains(n)) arrOut.add(n);
             }
--- Task3/Solution_01/src/ReadFromFile.java
+++ Task3/Solution_01/src/ReadFromFile.java
@@ -3,6 +3,34 @@ import java.util.ArrayList;
 
 public class ReadFromFile {
 
+    // вот метод, который можно вызывать вместо твоего read, и он схавает русскую кодировку.
+    // на stackOverflow нашёл по первой ссылке.
+    public static ArrayList<String> readCyrillic(String fileName) throws IOException {
+        ArrayList<String> arrStr = new ArrayList<String>();
+
+        File file = new File(fileName);
+        if (!file.exists()) {
+            System.out.println("Файл не существует");
+            System.exit(0);
+        }
+
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in,"windows-1251"));
+            try {
+                String s;
+                while ((s = br.readLine()) != null) {
+                    arrStr.add(s);
+                }
+            } finally {
+                br.close();
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return arrStr;
+    }
+
     public static ArrayList<String> read(String fileName) throws FileNotFoundException {
         ArrayList<String> arrStr = new ArrayList<String>();
 
@@ -10,6 +38,9 @@ public class ReadFromFile {
 
         if (!file.exists()) {
             System.out.println("Файл не существует");
+            // опасный код. От твоего метода будут ожидать, что он вернёт объект списка, а в определённых случаях он возвращает null.
+            // Так что нужно либо тут возвращать пустой список, либо во всех местах использования метода делать проверку объекта на null,
+            // что, как бы, тоже быстродействию не способствует. В данном примере следовало вывести сообщение и завершить программу.
             return null;
         }
 
